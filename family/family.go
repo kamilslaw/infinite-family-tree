2b54da6891df303d7ee67fb6429f986f00e6c85b
Kamil Gebarowski <k.gebarowski1@gmail.com>
2024-03-13 20:37:40 +0000

    basic addPerson func

--- family/family.go
+++ family/family.go
@@ -2,7 +2,15 @@ package family
 
 // TODO: more efficient way of storing the relationships - for now it should be fine (Unless your family has tens of thousands of members)
 
+// TODO: add implementation of interface to support the tree/graph visualisation/tracking (so the tree is generic and not family-tree dependent)
+
 type Family struct {
 	people    []Person
 	relations []Relationship
+}
+
+func (f *Family) AddPerson(p Person) error {
+	f.people = append(f.people, p)
+
+	return nil
 }
